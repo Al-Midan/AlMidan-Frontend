40f0f761578cdf6ed733dd46903fed9f83247d3f
Muhammed Easa <easanedumangad@gmail.com>
2024-08-04 12:59:12 +0530

    About Page Values Added

--- src/data/Content.tsx
+++ src/data/Content.tsx
@@ -1,78 +1,88 @@
- export const Content = [
-    {
-      title: "Lorem Ipsum Dolor Sit Amet",
-      description: (
-        <>
-          <p>
-            Sit duis est minim proident non nisi velit non consectetur. Esse
-            adipisicing laboris consectetur enim ipsum reprehenderit eu deserunt
-            Lorem ut aliqua anim do. Duis cupidatat qui irure cupidatat incididunt
-            incididunt enim magna id est qui sunt fugiat. Laboris do duis pariatur
-            fugiat Lorem aute sit ullamco. Qui deserunt non reprehenderit dolore
-            nisi velit exercitation Lorem qui do enim culpa. Aliqua eiusmod in
-            occaecat reprehenderit laborum nostrud fugiat voluptate do Lorem culpa
-            officia sint labore. Tempor consectetur excepteur ut fugiat veniam
-            commodo et labore dolore commodo pariatur.
-          </p>
-          <p>
-            Dolor minim irure ut Lorem proident. Ipsum do pariatur est ad ad
-            veniam in commodo id reprehenderit adipisicing. Proident duis
-            exercitation ad quis ex cupidatat cupidatat occaecat adipisicing.
-          </p>
-          <p>
-            Tempor quis dolor veniam quis dolor. Sit reprehenderit eiusmod
-            reprehenderit deserunt amet laborum consequat adipisicing officia qui
-            irure id sint adipisicing. Adipisicing fugiat aliqua nulla nostrud.
-            Amet culpa officia aliquip deserunt veniam deserunt officia
-            adipisicing aliquip proident officia sunt.
-          </p>
-        </>
-      ),
-      badge: "React",
-      image:
-        "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Lorem Ipsum Dolor Sit Amet",
-      description: (
-        <>
-          <p>
-            Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
-            deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
-            non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
-            sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
-            velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
-            commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
-          </p>
-          <p>
-            In dolore veniam excepteur eu est et sunt velit. Ipsum sint esse
-            veniam fugiat esse qui sint ad sunt reprehenderit do qui proident
-            reprehenderit. Laborum exercitation aliqua reprehenderit ea sint
-            cillum ut mollit.
-          </p>
-        </>
-      ),
-      badge: "Changelog",
-      image:
-        "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Lorem Ipsum Dolor Sit Amet",
-      description: (
-        <>
-          <p>
-            Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
-            deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
-            non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
-            sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
-            velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
-            commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
-          </p>
-        </>
-      ),
-      badge: "Launch Week",
-      image:
-        "https://images.unsplash.com/photo-1469474968028-56623f02e42e?auto=format&fit=crop&q=80&w=3506&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ];
-  
+export const Content = [
+  {
+    title: "Welcome to Al-Midan",
+    description: (
+      <>
+        <p>
+          Al-Midan, derived from the Arabic word for &quot;field,&quot; is a dynamic
+          freelance portal where skilled professionals and those in need of
+          their expertise come together. Whether you&apos;re a buyer seeking talented
+          individuals for tasks like website development or digital marketing,
+          or a seller offering your skills, Al-Midan is the perfect meeting
+          ground. 🌟
+        </p>
+      </>
+    ),
+    badge: "About Us",
+    image:
+      "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Buyer and Seller Opportunities",
+    description: (
+      <>
+        <p>
+          On Al-Midan, you can be both a buyer and a seller. As a seller,
+          showcase and sell your courses, connect with buyers via messaging or
+          video calls, and even conduct live streams for your course buyers. As
+          a buyer, you can find the perfect freelancer for your needs and access
+          a variety of courses to enhance your skills. 📚💼
+        </p>
+      </>
+    ),
+    badge: "Opportunities",
+    image:
+      "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Our Offerings",
+    description: (
+      <>
+        <p>
+          Al-Midan offers a range of services: buying or selling courses,
+          creating skill posts or sending proposals, posting jobs or applying
+          for jobs. There are no rigid roles; you have the flexibility to
+          participate in various activities.
+        </p>
+      </>
+    ),
+    badge: "Services",
+    image:
+      "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?auto=format&fit=crop&q=80&w=3506&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Course and Job Postings",
+    description: (
+      <>
+        <p>
+          Once a user creates a post or course, the admin will review and
+          approve or reject it. Approved posts appear in the feed. In the course
+          section, you can enroll in courses but cannot access the ones you
+          created. In the job section, if you want a job, you need to send a
+          proposal. The post owner can accept or reject it, and the same process
+          applies to skill posts.
+        </p>
+      </>
+    ),
+    badge: "Postings",
+    image:
+      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?auto=format&fit=crop&q=80&w=3506&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Messaging and Payments",
+    description: (
+      <>
+        <p>
+          If the post owner accepts your proposal, you can message and negotiate
+          the details. Set a price and handle payments through the chat, similar
+          to platforms like OLX. You can send offers, and if accepted, the
+          transaction proceeds through the Razorpay interface. This is how
+          Al-Midan works.
+        </p>
+      </>
+    ),
+    badge: "Messaging & Payments",
+    image:
+      "https://images.unsplash.com/photo-1464457312035-3d7d0e0c058e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
