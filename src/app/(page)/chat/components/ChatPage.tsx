672166bce58c2ef8dd0f97915d083e2a8e9ef229
Muhammed Easa <easanedumangad@gmail.com>
2024-08-19 21:51:59 +0530

    Chat Page Socket Url Changed

--- src/app/(page)/chat/components/ChatPage.tsx
+++ src/app/(page)/chat/components/ChatPage.tsx
@@ -61,8 +61,8 @@ const ChatPage: React.FC = () => {
 
     fetchAcceptedProposals();
 
-    const socketUrl =
-      process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5004";
+    const socketUrl ="http://13.71.112.129/freelance-service"
+      //process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5004";
     const newSocket = io(socketUrl, {
       transports: ["websocket", "polling"],
     });
